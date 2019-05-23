b2103144ea2b34d955d8b0a80d1e045fd1ff7f2e
David Ramlakhan <dram1964@gmail.com>
2019-05-23 16:04:16 +0100

    Modified conf import in openehr_rest

--- app/openehr_rest.py
+++ app/openehr_rest.py
@@ -10,13 +10,9 @@ from values stored in the openehr_conf module
 """
 
 import urllib.request
-import openehr_conf
+from openehr_conf import service_url, user_name, password
 
 requestor = urllib.request
-
-service_url = openehr_conf.service_url
-user_name = openehr_conf.user_name
-password = openehr_conf.password
 
 password_mgr = requestor.HTTPPasswordMgrWithDefaultRealm()
 password_mgr.add_password( 'Think!EHR', service_url, user_name, password )
