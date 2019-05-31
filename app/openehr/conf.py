8e21301d39dbc9448edc7a19f8ae5237a18d24e0
David Ramlakhan <dram1964@gmail.com>
2019-05-31 15:13:17 +0100

    Configured openehr.conf module for dual-mode usage

--- app/openehr/conf.py
+++ app/openehr/conf.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 """
-openehr_conf module
-
-Holds configuration parameters for the target EHR server
+File: openehr.conf
+Package module holding configuration settings for the
+openehr package. 
+Package can be run from the command line:
+python openehr/conf.py [setting[0],...,setting[n]
+With no arguments returns a list of configured settings
+With arguments returns the value for the setting
 """
 
 test_ehrid = '33b11502-9e5c-41f0-9cea-93b851c85b67'
@@ -12,3 +16,16 @@ test_subject_id = '3333333333'
 service_url = 'http://thinkehr:8081/rest/v1/'
 user_name  = 'admin'
 password = 'admin'
+
+if __name__ == '__main__':
+    import sys
+    import conf
+    if len( sys.argv ) == 1:
+        print('Configured attributes: ')
+        print( [attr for attr in conf.__dict__ if not attr.startswith('__')] )
+    else:
+        for attr in sys.argv[1:]:
+            if attr in conf.__dict__:
+                print('%s is %s' % (attr, getattr(conf, attr)))
+            else:
+                print('Unconfigured setting %s' % attr)
