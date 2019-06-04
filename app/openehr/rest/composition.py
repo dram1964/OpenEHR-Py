68ccc1e3d8b8d4e5df73b585248285e96c13fbae
David Ramlakhan <dram1964@gmail.com>
2019-06-04 13:07:53 +0100

    Refactored rest/composition to use run_rest_query from rest/requestor module

--- app/openehr/rest/composition.py
+++ app/openehr/rest/composition.py
@@ -13,7 +13,7 @@ value defined in the system configuration file (openehr/conf.py).
 import urllib.parse, urllib.error
 import json
 from openehr.conf import service_url, test_uid
-from openehr.rest.requestor import get_requestor as _get_requestor
+from openehr.rest.requestor import run_rest_query
 
 def get_composition_by_uid( composition_id=test_uid, response_format='FLAT', include_meta='true', debug=1):
     """
@@ -28,24 +28,9 @@ def get_composition_by_uid( composition_id=test_uid, response_format='FLAT', inc
 
     url = service_url + 'composition/' + composition_id + '?' +  url_values
     if debug: print('Retrieving', url)
-    requestor = _get_requestor()
 
-    try:
-        response = requestor.urlopen( url )
-    except urllib.error.HTTPError as e:
-        print( 'HTTP Error: ', e.code )
-        print( 'Reason: ', e.reason )
-        print( 'Headers: ', e.headers )
-    else:
-        data = response.read().decode()
-        if debug: print( 'Response Code: %s' % response.getcode() )
-        if debug: print( 'Retrieved %s characters' % len(data) )
-
-        try:
-            composition = json.loads(data)
-            return composition
-        except:
-            return None
+    response = run_rest_query(url)
+    return response
 
 
 if __name__ == '__main__':
